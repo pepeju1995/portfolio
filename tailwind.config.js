40ac8c4c102ae216eedcb83c7fe33d6eb7ece76a
Jose Juan Perez <pepeju95@gmail.com>
2023-04-20 07:59:41 +0200

    Add/modify configurations

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
